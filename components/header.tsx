7dedf003f754f2471013c50b3a633b80ccf7b2a6
Bastiaan Quast <bquast@gmail.com>
2025-04-17 04:50:52 -0600

    add GA

--- components/header.tsx
+++ components/header.tsx
@@ -8,6 +8,14 @@ import { ModeToggle } from "@/components/mode-toggle"
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6LEBXCQDME"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6LEBXCQDME');
+          </script>
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Shield className="h-8 w-8 text-primary" />
