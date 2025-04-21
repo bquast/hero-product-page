066e96418598bbede0500cced5fe57fe4a4a8e6c
Bastiaan Quast <bquast@gmail.com>
2025-04-21 09:54:33 -0600

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                Transform Your Marketing with StoryBrand
+                Transform Your Marketing with HERO marketing
               </h1>
               <p className="max-w-[600px] text-muted-foreground md:text-xl">
                 Clarify your message so customers will listen. Create compelling narratives that convert using the
